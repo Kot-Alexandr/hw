4b9ffa9a57f12189f068c3e13eaa99e7d5f98d48
Kot-Alexandr <voollkot@gmail.com>
2023-01-08 21:08:28 +0300

    add tas

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -18,18 +18,23 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+                {/*<Route ...*/} 
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                <Route path={'/'} element={<Navigate to={'/pre-junior'}/>} />
+                <Route path={'/pre-junior'} element={<PreJunior />} />
+                <Route path={'/junior'} element={<Junior />} />
+                <Route path={'/junior-plus'} element={<JuniorPlus />} />
+                <Route path={'/*'} element={<Error404 />} />
             </Routes>
         </div>
     )
 }
 
 export default Pages
+
+{/*роуты для /pre-junior, /junior, /junior-plus*/ }
+{/*<Route ...*/ }
+{/*<Route ...*/ }
+{/*<Route ...*/ }
+{/*роут для несуществующей страницы должен отрисовать <Error404 />*/ }
+{/*<Route ...*/ }
